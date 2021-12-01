51be02e102f6cff67003616ff95db90d76dc1031
Robert Griffin <r.maurice.griffin@gmail.com>
2021-12-01 18:19:17 -0500

    Tests downloading and working with netCDF files

---
+++ NA_CORDEXtest.R
@@ -0,0 +1,21 @@
+## Use this to start every program.  This clears out previous information from memory
+rm(list=ls())
+
+## Initalize renv for library lockfile
+library(renv)
+#renv::init()
+
+## Packages
+#Sys.setenv(RENV_PATHS_RTOOLS = "C:/rtools40/") # https://github.com/rstudio/renv/issues/225
+
+PKG <- c("sf","tidyverse","rgdal")
+
+for (p in PKG) {
+  if(!require(p,character.only = TRUE)) {  
+    install.packages(p)
+    require(p,character.only = TRUE)}
+}
+rm(p,PKG)
+
+## Snapshot of libraries used
+renv::snapshot()
